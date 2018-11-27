a9ae4616e796fdde46a655e70c90d771b74a31ca
Monika Markowska <monika.nesteruk@gmail.com>
2018-11-27 13:03:39 +0100

    Intagration vue.js app with Firebase

--- blog/src/main.js
+++ blog/src/main.js
@@ -1,9 +1,24 @@
 import Vue from 'vue'
 import App from './App.vue'
+import * as firebase from 'firebase'
+import {store} from './store/store'
 import VueRouter from 'vue-router'
 import Routes from './routes'
-// import {store} from './store/store'
 
+ // Initialize Firebase
+ const config = {
+  apiKey: "AIzaSyDDtRGGH2trwTrHh0ijITiXi14W0kD3pv8",
+  authDomain: "portfolio-blog-c7083.firebaseapp.com",
+  databaseURL: "https://portfolio-blog-c7083.firebaseio.com",
+  projectId: "portfolio-blog-c7083",
+  storageBucket: "portfolio-blog-c7083.appspot.com",
+  messagingSenderId: "177453477998"
+};
+
+firebase.initializeApp(config);
+Vue.prototype.$firebase = firebase;
+
+//Routing
 Vue.use(VueRouter)
 
 const router = new VueRouter({
@@ -12,12 +27,10 @@ const router = new VueRouter({
   });
 
 
-
-
 Vue.config.productionTip = false
 
 new Vue({
-  // store: store,
+  store: store,
   router: router,
   render: h => h(App),
 }).$mount('#app')
--- blog/src/store/store.js
+++ blog/src/store/store.js
@@ -1,11 +1,19 @@
+import Vue from 'vue';
+import Vuex from 'vuex';
+import * as firebase from 'firebase'
 
 Vue.use(Vuex);
 
 export const store = new Vuex.Store({
-    state: {
-
+    state: {},
+    mutations: {},
+    actions: {
+        myFirebaseAction: ({commit}) => {
+            //you can use firebase like this
+            var ref = firebase.database().ref()
+        }
     }
-})
+});
 
 // const createStore = () => {
     
