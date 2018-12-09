4a1281a65a4351955f7d5726999f568fbf77d17a
Monika Markowska <monika.nesteruk@gmail.com>
2018-12-09 12:43:02 +0100

    added route for github pages

--- blog/src/routes.js
+++ blog/src/routes.js
@@ -4,6 +4,7 @@ import SinglePost from "./components/SinglePost.vue"
 
 export default [
     {path:"/", redirect: "/posts/"},
+    {path:"/portfolio", redirect: "/posts/"}, //for GitHub pages
     {path:"/posts", component: LatestPosts, props: { category: null }},
     {path:"/posts/:category", component: LatestPosts, props: true},
     {path:"/post/:id", component: SinglePost, props: (route) => ({ id: parseInt(route.params.id) })},
